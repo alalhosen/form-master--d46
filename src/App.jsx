a1b62d9b84ec5cb4b13c188a0f975c7b68abe7c6
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-02 22:55:37 +0600

    handleLocalSubmit function set

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import "./App.css";
 // import HookForm from './components/HookForm/HookForm'
-import ReusableForm from './components/ReusableForm/ReusableForm'
+import ReusableForm from "./components/ReusableForm/ReusableForm";
 // import RefForm from './components/RefForm/RefForm'
 // import SimpleForm from './components/SimpleForm/SimpleForm'
 // import StateFulForm from './components/StateFulForm/StateFulForm'
 
 function App() {
+  const handleSignUpSubmit = (data) => {
+    console.log("sign up data", data);
+  };
+  const handleUpdateProfile = (data) => {
+    console.log("update profile data", data);
+  };
 
   return (
     <>
@@ -14,10 +20,17 @@ function App() {
       {/* <StateFulForm></StateFulForm> */}
       {/* <RefForm></RefForm> */}
       {/* <HookForm></HookForm> */}
-      <ReusableForm></ReusableForm>
-      
+      <ReusableForm
+        formTitle={"Sign Up"}
+        handleSubmit={handleSignUpSubmit}
+      ></ReusableForm>
+      <ReusableForm
+        formTitle={"Profile Update"}
+        handleSubmit={handleUpdateProfile}
+        submitBtnText="Update"
+      ></ReusableForm>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ReusableForm/ReusableForm.jsx
+++ src/components/ReusableForm/ReusableForm.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 
-const ReusableForm = () => {
-    const handleSubmit= e=>{
-        e.preventDefault();
-    }
+const ReusableForm = ({
+  formTitle,
+  handleSubmit,
+  submitBtnText = "Submit",
+}) => {
+  const handleLocalSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+    handleSubmit(data);
+  };
   return (
     <div>
-        <h2>ReUsableForm</h2>
-      <form onSubmit={handleSubmit}>
+      <h2>{formTitle}</h2>
+      <form onSubmit={handleLocalSubmit}>
         <input type="text" name="name" />
         <br />
         <input type="email" name="email" />
         <br />
         <input type="password" name="password" />
         <br />
-        <input type="submit" value="Submit" />
+        <input type="submit" value={submitBtnText} />
       </form>
     </div>
   );
