c0c8d273c9106a71547cf7d90fada1041ec30460
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-03 23:10:44 +0600

    asset give to diamond in special

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -4,12 +4,13 @@ import Uncle from "../Uncle/Uncle";
 import './Grandpa.css'
 
 const Grandpa = () => {
+    const asset='diamond';
     return (
         <div className="grandpa">
             <h2>Grandpa</h2>
             <section className="flex">
-            <Dad></Dad>
-            <Uncle></Uncle>
+            <Dad asset={asset}></Dad>
+            <Uncle asset={asset}></Uncle>
             <Aunty></Aunty>
             </section>
         </div>
