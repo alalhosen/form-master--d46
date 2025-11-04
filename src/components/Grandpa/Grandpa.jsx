5a0068d9cca8c7cadfd4f7dc51678bea43f6e38e
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-04 17:52:43 +0600

    master form has completed

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,23 +1,28 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import "./Grandpa.css";
 
 export const AssetContext = createContext("gold");
+export const MoneyContext = createContext(1000);
 
 const Grandpa = () => {
+    const [money, setMoney]=useState(1000);
   const asset = "diamond";
   return (
     <div className="grandpa">
       <h2>Grandpa</h2>
-      <AssetContext.Provider value="gold">
-        <section className="flex">
-          <Dad asset={asset}></Dad>
-          <Uncle asset={asset}></Uncle>
-          <Aunty></Aunty>
-        </section>
-      </AssetContext.Provider>
+      <p>Net Money: {money}</p>
+      <MoneyContext value={[money, setMoney]}>
+        <AssetContext.Provider value="gold">
+          <section className="flex">
+            <Dad asset={asset}></Dad>
+            <Uncle asset={asset}></Uncle>
+            <Aunty></Aunty>
+          </section>
+        </AssetContext.Provider>
+      </MoneyContext>
     </div>
   );
 };
